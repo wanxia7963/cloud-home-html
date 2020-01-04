8e6ba45ee7b7d1b157c5dbefba1b1010159ee334
wanxia <826660864@qq.com>
2020-01-04 18:32:07 +0800

    123

--- src/page/Home/index.vue
+++ src/page/Home/index.vue
@@ -2,31 +2,34 @@
     <div class="Home-Page">
         <HeaderNavigation :anchor="homeAnchor" @toAnchorByOffsetTop="toAchar"></HeaderNavigation>
         <div ref="one" class="Home-Page-div Home-Page-first">
+            <canvas id="canvas" width="1950px" height="968px;"></canvas>
+            <canvas id="canvasbg" width="1950px" height="968px"></canvas>
             <div class="Home-Page-first-content">
-                <div class="left animated fadeInLeft">
-                    <div class="left-content one">
-                        <span class="title">邻家平台APP</span>
-                    </div>
-                    <div class="left-content two">
-                        <span class="message">分享社交电商新领袖，汇聚海量网购优惠券</span>
-                    </div>
-                    <div class="left-content three">
-                        <div class="three-erweima-img">
-                            <img src="../../img/code.png" alt="">
-                            <img style="margin-left:35px;width:240px;" src="../../img/1(1).png" alt="">
+                    <div class="left animated fadeInLeft">
+                        <div class="left-content one">
+                            <span class="title">邻家平台APP</span>
+                        </div>
+                        <div class="left-content two">
+                            <span class="message">分享社交电商新领袖，汇聚海量网购优惠券</span>
+                        </div>
+                        <div class="left-content three">
+                            <div class="three-erweima-img">
+                                <img src="../../img/code.png" alt="">
+                                <img style="margin-left:35px;width:240px;" src="../../img/1(1).png" alt="">
+                            </div>
+                        </div>
+                        <div class="left-content four">
+                            <div class="four-download-img">
+                                <img @mouseover="mouseAndroid()" @mouseout="mouseoutAndroid()" style="margin-right:20px;" :src="androidUrl" alt="">
+                                <img @mouseover="mouseIos()" @mouseout="mouseoutIos()" :src="iosUrl" alt="">
+                            </div>
                         </div>
                     </div>
-                    <div class="left-content four">
-                        <div class="four-download-img">
-                            <img @mouseover="mouseAndroid()" @mouseout="mouseoutAndroid()" style="margin-right:20px;" :src="androidUrl" alt="">
-                            <img @mouseover="mouseIos()" @mouseout="mouseoutIos()" :src="iosUrl" alt="">
-                        </div>
+                    <div class="right animated fadeInRight">
+                        <img style="width:590px;margin-left:70px;" src="../../img/phone.png" alt="">
                     </div>
                 </div>
-                <div class="right animated fadeInRight">
-                    <img style="width:590px;margin-left:70px;" src="../../img/phone.png" alt="">
-                </div>
-            </div>
+            
         </div>
         <div ref="two" class="Home-Page-div Home-Page-second">
             <div class="Home-Page-second-content">
@@ -406,6 +409,8 @@ import android from '../../img/android.png'
 import ios_no from '../../img/ios1.png'
 import ios from '../../img/ios.png'
 import $ from 'jquery'
+
+
 export default {
     name:'home',
     components:{
@@ -430,15 +435,18 @@ export default {
 
             current: 0,
             currentCard:0,
-
             homeAnchor:1,
-
             androidUrl: android_no,
-            iosUrl: ios_no
+            iosUrl: ios_no,
+            
         }
     },
     mounted(){
-        window.addEventListener('scroll',this.handleScroll)
+       this.$nextTick(()=>{
+            window.console.log("11s"+document.getElementById('canvas'))
+       })
+    window.addEventListener('scroll',this.handleScroll)
+    canvasTD()
     },
     methods:{
         handleScroll(){
@@ -571,6 +579,190 @@ export default {
         window.removeEventListener('scroll', this.handleScroll); // 离开页面 关闭监听 不然会报错
     }
 }
+
+function canvasTD(){
+var radMin = 5,
+    radMax = 50,
+    filledCircle = 40, //percentage of filled circles
+    concentricCircle = 20, //percentage of concentric circles
+    radThreshold = 20; //IFF special, over this radius concentric, otherwise filled
+
+    //min and max speed to move
+    var speedMin = 0.3,
+    speedMax = 2.5;
+
+    //max reachable opacity for every circle and blur effect
+    var maxOpacity = 0.6;
+    //default palette choice
+    var colors = ['255,255,255'],
+    bgColors = ['255,255,255'],
+    circleBorder = 8,
+    backgroundLine = bgColors[0];
+    var backgroundMlt = 0.85;
+    var canvas = document.getElementById('canvas')
+    window.console.log(canvas)
+    //min distance for links
+    var linkDist = Math.min(canvas.width, canvas.height) / 3,
+
+    lineBorder = 2.5;
+    //most importantly: number of overall circles and arrays containing them
+    var maxCircles = 5,
+    points = [],
+    pointsBack = [];
+
+    //populating the screen
+    for (var i = 0; i < maxCircles * 2; i++) points.push(new Circle());
+    for (var i = 0; i < maxCircles; i++) pointsBack.push(new Circle(true));
+
+    //experimental vars
+    var circleExp = 1,
+    circleExpMax = 1.003,
+    circleExpMin = 0.997,
+    circleExpSp = 0.00004,
+    circlePulse = false;
+
+    //circle class
+    function Circle(background) {
+    //if background, it has different rules
+    this.background = (background || false);
+    this.x = randRange(-canvas.width / 2, canvas.width / 2);
+    this.y = randRange(-canvas.height / 2, canvas.height / 2);
+    this.radius = background ? hyperRange(radMin, radMax) * backgroundMlt : hyperRange(radMin, radMax);
+    this.filled = this.radius < radThreshold ? (randint(0, 100) > filledCircle ? false : 'full') : (randint(0, 100) > concentricCircle ? false : 'concentric');
+    this.color = background ? bgColors[randint(0, bgColors.length - 1)] : colors[randint(0, colors.length - 1)];
+    this.borderColor = background ? bgColors[randint(0, bgColors.length - 1)] : colors[randint(0, colors.length - 1)];
+    this.opacity = 0.05;
+    this.speed = (background ? randRange(speedMin, speedMax) / backgroundMlt : randRange(speedMin, speedMax)); // * (radMin / this.radius);
+    this.speedAngle = Math.random() * 2 * Math.PI;
+    this.speedx = Math.cos(this.speedAngle) * this.speed;
+    this.speedy = Math.sin(this.speedAngle) * this.speed;
+    var spacex = Math.abs((this.x - (this.speedx < 0 ? -1 : 1) * (canvas.width / 2 + this.radius)) / this.speedx),
+        spacey = Math.abs((this.y - (this.speedy < 0 ? -1 : 1) * (canvas.height / 2 + this.radius)) / this.speedy);
+    this.ttl = Math.min(spacex, spacey);
+    };
+
+    Circle.prototype.init = function() {
+    Circle.call(this, this.background);
+    }
+
+    //support functions
+    //generate random int a<=x<=b
+    function randint(a, b) {
+        return Math.floor(Math.random() * (b - a + 1) + a);
+    }
+    //generate random float
+    function randRange(a, b) {
+        return Math.random() * (b - a) + a;
+    }
+    //generate random float more likely to be close to a
+    function hyperRange(a, b) {
+    return Math.random() * Math.random() * Math.random() * (b - a) + a;
+    }
+
+    //rendering function
+    function drawCircle(ctx, circle) {
+    //circle.radius *= circleExp;
+    var radius = circle.background ? circle.radius *= circleExp : circle.radius /= circleExp;
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, radius * circleExp, 0, 2 * Math.PI, false);
+    ctx.lineWidth = Math.max(1, circleBorder * (radMin - circle.radius) / (radMin - radMax));
+    ctx.strokeStyle = ['rgba(', circle.borderColor, ',', circle.opacity, ')'].join('');
+    if (circle.filled == 'full') {
+        ctx.fillStyle = ['rgba(', circle.borderColor, ',', circle.background ? circle.opacity * 0.8 : circle.opacity, ')'].join('');
+        ctx.fill();
+        ctx.lineWidth=0;
+        ctx.strokeStyle = ['rgba(', circle.borderColor, ',', 0, ')'].join('');
+    }
+    ctx.stroke();
+    if (circle.filled == 'concentric') {
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, radius / 2, 0, 2 * Math.PI, false);
+        ctx.lineWidth = Math.max(1, circleBorder * (radMin - circle.radius) / (radMin - radMax));
+        ctx.strokeStyle = ['rgba(', circle.color, ',', circle.opacity, ')'].join('');
+        ctx.stroke();
+    }
+    circle.x += circle.speedx;
+    circle.y += circle.speedy;
+    if (circle.opacity < (circle.background ? maxOpacity : 1)) circle.opacity += 0.01;
+    circle.ttl--;
+    }
+
+    //initializing function
+    function init() {
+    window.requestAnimationFrame(draw);
+    }
+
+    //rendering function
+    function draw() {
+
+    if (circlePulse) {
+        if (circleExp < circleExpMin || circleExp > circleExpMax) circleExpSp *= -1;
+        circleExp += circleExpSp;
+    }
+    var ctxfr = document.getElementById('canvas').getContext('2d');
+    var ctxbg = document.getElementById('canvasbg').getContext('2d');
+
+    ctxfr.globalCompositeOperation = 'destination-over';
+    ctxfr.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+    ctxbg.globalCompositeOperation = 'destination-over';
+    ctxbg.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+
+    ctxfr.save();
+    ctxfr.translate(canvas.width / 2, canvas.height / 2);
+    ctxbg.save();
+    ctxbg.translate(canvas.width / 2, canvas.height / 2);
+
+    //function to render each single circle, its connections and to manage its out of boundaries replacement
+    function renderPoints(ctx, arr) {
+        for (var i = 0; i < arr.length; i++) {
+        var circle = arr[i];
+        //checking if out of boundaries
+        if (circle.ttl<0) {}
+        var xEscape = canvas.width / 2 + circle.radius,
+            yEscape = canvas.height / 2 + circle.radius;
+        if (circle.ttl < -20) arr[i].init(arr[i].background);
+        //if (Math.abs(circle.y) > yEscape || Math.abs(circle.x) > xEscape) arr[i].init(arr[i].background);
+        drawCircle(ctx, circle);
+        }
+        for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            var deltax = arr[i].x - arr[j].x;
+            var deltay = arr[i].y - arr[j].y;
+            var dist = Math.pow(Math.pow(deltax, 2) + Math.pow(deltay, 2), 0.5);
+            //if the circles are overlapping, no laser connecting them
+            if (dist <= arr[i].radius + arr[j].radius) continue;
+            //otherwise we connect them only if the dist is < linkDist
+            if (dist < linkDist) {
+            var xi = (arr[i].x < arr[j].x ? 1 : -1) * Math.abs(arr[i].radius * deltax / dist);
+            var yi = (arr[i].y < arr[j].y ? 1 : -1) * Math.abs(arr[i].radius * deltay / dist);
+            var xj = (arr[i].x < arr[j].x ? -1 : 1) * Math.abs(arr[j].radius * deltax / dist);
+            var yj = (arr[i].y < arr[j].y ? -1 : 1) * Math.abs(arr[j].radius * deltay / dist);
+            ctx.beginPath();
+            ctx.moveTo(arr[i].x + xi, arr[i].y + yi);
+            ctx.lineTo(arr[j].x + xj, arr[j].y + yj);
+            var samecolor = arr[i].color == arr[j].color;
+            ctx.strokeStyle = ["rgba(", arr[i].borderColor, ",", Math.min(arr[i].opacity, arr[j].opacity) * ((linkDist - dist) / linkDist), ")"].join("");
+            ctx.lineWidth = (arr[i].background ? lineBorder * backgroundMlt : lineBorder) * ((linkDist - dist) / linkDist); //*((linkDist-dist)/linkDist);
+            ctx.stroke();
+            }
+        }
+        }
+    }
+
+    var startTime = Date.now();
+    renderPoints(ctxfr, points);
+    renderPoints(ctxbg, pointsBack);
+    var deltaT = Date.now() - startTime;
+
+    ctxfr.restore();
+    ctxbg.restore();
+
+    window.requestAnimationFrame(draw);
+    }
+
+    init();
+    
+}
 </script>
 <style scoped>
 .animated {animation-duration: 2s;}
@@ -580,12 +772,29 @@ export default {
     background-size:cover;
     width:100%;
     height: 968px; 
+    display: flex;
+    justify-content: center;
+}
+#canvas {
+  z-index: 1;
+  position: absolute
+}
+#canvasbg {
+  z-index: -10;
+  -webkit-filter: blur(3px);
+  -moz-filter: blur(3px);
+  -o-filter: blur(3px);
+  filter: blur(3px);
+  opacity: 0.6;
+  position: absolute
 }
 .Home-Page-first-content{
     padding-top: 222px;
     width: 1200px;
     margin:0 auto;
     display: flex;
+    position: absolute;
+    z-index: 10;
 }
 /* .Home-Page-first-content .left{} */
 .left .one .title{
